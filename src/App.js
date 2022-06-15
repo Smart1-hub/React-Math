10cb88eb51a743985afbbbf051f2217a921101cc
Ademola Adebayo <scdk@yahoo.com>
2022-06-15 09:39:50 +0100

    Install dependency

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
-import Calculator from './components/Calculator';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Calculators from './pages/Calculators,';
+import Quote from './pages/Quote';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = '';
   }
 
   render() {
     return (
-      <div className="container">
-        <Calculator />
+
+      <div className="App">
+
+        <nav>
+          <h1 className="logo">Math Magicians</h1>
+          <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/calculator">Calculator</Link>
+            <Link to="/quotes">Quote</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculators />} />
+          <Route path="/quotes" element={<Quote />} />
+        </Routes>
+
       </div>
     );
   }
 }
-
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,40 +1,30 @@
-import { useState } from 'react';
-import './Calculator.css';
+import React, { useState } from 'react';
+import '../App.css';
+import Buttons from './Buttons';
+import Output from './Output';
 import calculate from '../logic/calculate';
 
 const Calculator = () => {
-  const [state, setState] = useState({ total: null, next: null, operation: null });
-  const { total, next, operation } = state;
+  const [state, setState] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
 
-  const executeCalculation = (event) => {
-    const calcButtons = event.target.innerText;
-    const calculatorButton = calcButtons;
-    const obj = state;
-    setState(calculate(obj, calculatorButton));
+  const clickHandler = (event) => {
+    setState({ ...state, ...calculate(state, event) });
   };
 
   return (
-    <div className="calc-container">
-      <div className="output">{ next || operation || total || 0 }</div>
-      <button type="button" onClick={executeCalculation}>AC</button>
-      <button type="button" onClick={executeCalculation}>+/-</button>
-      <button on type="button" onClick={executeCalculation}>%</button>
-      <button type="button" className="br operators" onClick={executeCalculation}>÷</button>
-      <button type="button" onClick={executeCalculation}>7</button>
-      <button type="button" onClick={executeCalculation}>8</button>
-      <button on type="button" onClick={executeCalculation}>9</button>
-      <button type="button" className="operators" onClick={executeCalculation}>x</button>
-      <button type="button" onClick={executeCalculation}>4</button>
-      <button type="button" onClick={executeCalculation}>5</button>
-      <button on type="button" onClick={executeCalculation}>6</button>
-      <button type="button" className="operators" onClick={executeCalculation}>-</button>
-      <button type="button" onClick={executeCalculation}>1</button>
-      <button type="button" onClick={executeCalculation}>2</button>
-      <button type="button" className="span-t-b" onClick={executeCalculation}>3</button>
-      <button type="button" className="operators" onClick={executeCalculation}>+</button>
-      <button type="button" className="zero-span2" onClick={executeCalculation}>0</button>
-      <button type="button" onClick={executeCalculation}>.</button>
-      <button type="button" className="operators" onClick={executeCalculation}>=</button>
+    <div className="calculator-grid">
+      <Output
+        total={state.total}
+        operation={state.operation}
+        next={state.next}
+      />
+      <Buttons
+        clickHandler={clickHandler}
+      />
     </div>
   );
 };
